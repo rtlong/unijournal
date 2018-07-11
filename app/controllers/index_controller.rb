405553c928b469020578c38d1cc0f275cb1b0f0d
Ryan Taylor Long <ryan@rtlong.com>
2018-07-11 12:09:05 -0700

    Add minimal rails code

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,15 @@
+class IndexController < ApplicationController
+  def index
+    posts = Post.limit(10).order(:created_at).to_a
+    @client_state = {
+      posts: posts.map { |p|
+        {
+          body: p.body,
+          timestamp: p.created_at,
+        }
+      },
+    }
+  end
+  attr_reader :client_state
+  helper_method :client_state
+end
