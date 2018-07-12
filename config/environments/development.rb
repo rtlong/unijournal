354bb9602b4023d2b6b9998687261ad815b0e67b
Ryan Taylor Long <ryan@rtlong.com>
2018-07-11 22:40:45 -0700

    Clean up ELM bits in config/webpack/environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -53,6 +51,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+  config.webpacker.check_yarn_integrity = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,2 @@
 const { environment } = require('@rails/webpacker')
-const elm =  require('./loaders/elm')
-
-environment.loaders.append('elm', elm)
 module.exports = environment
