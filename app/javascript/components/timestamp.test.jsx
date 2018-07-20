e4041f3bd3c7f95e2820f242794d6cfb80cad642
Ryan Taylor Long <ryan@rtlong.com>
2018-07-20 00:00:15 -0700

    Increase coverage of and refactor Timestamp test

--- app/javascript/components/timestamp.test.jsx
+++ app/javascript/components/timestamp.test.jsx
@@ -2,11 +2,14 @@ import expect from 'expect'
 import React from 'react'
 import { shallow } from 'enzyme'
 import moment from 'moment'
-import delay from 'delay'
 
 import Timestamp from './timestamp'
 
 describe('components/Timestamp', () => {
+  const component = props => (
+    <Timestamp {...props} />
+  )
+
   describe('displays the moment.js relative string', () => {
     [
       moment(),
@@ -17,32 +20,59 @@ describe('components/Timestamp', () => {
       moment().subtract(5, 'days'),
     ].forEach(timestamp => {
       test(timestamp.fromNow(), () => {
-        expect.assertions(3)
-        const component = shallow(
-          <Timestamp timestamp={timestamp} />,
-          { disableLifecycleMethods: true },
-        )
-        const el = component.find('abbr.timestamp')
+        const wrapper = shallow(component({ timestamp }), { disableLifecycleMethods: true })
+        const el = wrapper.find('abbr.timestamp')
         expect(el).toBeTruthy()
         expect(el.render().attr('title')).toBe(timestamp.toString())
-        expect(component.text()).toBe(timestamp.fromNow())
+        expect(wrapper.text()).toBe(timestamp.fromNow())
       })
     })
   })
 
-  test('updates itself', async () => {
+  test('handles updates to props', () => {
+    const timestamp0 = new Date()
+    const timestamp1 = new Date(Date.now() - 60000) // 60 seconds ago
+    const wrapper = shallow(component({ timestamp: timestamp0 }))
+
+    expect(wrapper.text()).toBe('a few seconds ago')
+    wrapper.setProps({
+      timestamp: timestamp1,
+    })
+    expect(wrapper.text()).toBe('a minute ago')
+  })
+
+  test('updates itself', () => {
+    jest.useFakeTimers()
+    let now = Date.now()
+    jest.spyOn(Date, 'now').mockImplementation(() => now)
+
     const thresh = moment.relativeTimeThreshold('s')
-    const timestamp = moment().subtract(thresh - 1, 'seconds')
+    const timestamp = moment(now).subtract(thresh - 1, 'seconds')
+    const wrapper = shallow(component({ timestamp }))
 
-    const component = shallow(<Timestamp timestamp={timestamp} />)
+    const text = wrapper.text()
 
-    const text0 = component.text()
+    const tickLength = 1000
+    now += tickLength // advance time by 1s
+    jest.advanceTimersByTime(tickLength) // run the timeouts the component sets up
+    wrapper.update() // force update Enzyme's view of component
 
-    await delay(1100) // wait over a second. moment output will be different and tick() should have occured to update the component
-    component.update() // ask Enzyme to update it's rendered state, which seems neccesary for some reason
+    expect(wrapper.text()).not.toBe(text)
+  })
 
-    expect(text0).not.toBe(component.text())
+  test('cleans up it\'s timers at unmount', () => {
+    const timestamp = new Date()
+    const wrapper = shallow(component({ timestamp }))
 
-    component.unmount() // unmount to clearInterval so test doesn't hang
+    jest.spyOn(global, 'clearTimeout')
+    const { timerId } = wrapper.instance()
+    wrapper.unmount()
+    expect(global.clearTimeout).toHaveBeenCalledTimes(1)
+    expect(global.clearTimeout).toHaveBeenCalledWith(timerId)
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+    jest.useRealTimers()
   })
 })
