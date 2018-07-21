71fac6546bb32b1e9040f262b63af08b3582b54d
Ryan Taylor Long <ryan@rtlong.com>
2018-07-21 01:49:57 -0700

    Move postJSON into new utils dir

--- app/javascript/actions.js
+++ app/javascript/actions.js
@@ -1,6 +1,6 @@
 import fetch from "cross-fetch"
 
-import { csrfToken } from "./rails"
+import postJSON from "./util/post_json"
 import * as Posts from "./entities/posts"
 import {
   NEW_POST_FORM_EXPAND,
@@ -32,23 +32,6 @@ export function newPostSourceChanged(payload) {
     type: NEW_POST_FORM_SOURCE_CHANGED,
     payload,
   }
-}
-
-function postJSON(url, data) {
-  // Default options are marked with *
-  return fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, cors, *same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, same-origin, *omit
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "X-CSRF-Token": csrfToken(),
-    },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
-    body: JSON.stringify(data), // body data type must match 'Content-Type' header
-  })
 }
 
 export function showMessage(message, type = "info", timeout = 10000) {
