6bfa5edad7d8525a54f69f19904cafab84c4b5bb
Ryan Taylor Long <ryan@rtlong.com>
2018-07-29 14:31:03 -0700

    Clean up main.jsx

--- frontend/src/packs/main.jsx
+++ frontend/src/packs/main.jsx
@@ -1,17 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
+import * as git from "isomorphic-git"
+import fs from "fs"
+import pify from "pify"
 
 import createStore from "../store"
 import App from "../app"
 import * as actions from "../actions"
 
-const store = createStore()
-
-global.actions = actions
-global.store = store
-
-const mountApp = () => {
+function mountApp(store) {
   ReactDOM.render(
     <Provider store={store}>
       <App />
@@ -20,59 +18,70 @@ const mountApp = () => {
   )
 }
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", mountApp)
-} else {
-  mountApp()
-}
-
-/* global BrowserFS */
-BrowserFS.configure(
-  {
-    fs: "IndexedDB",
-    options: {},
-  },
-  err => {
-    if (err) {
-      store.dispatch(
-        actions.showError(
-          `Error configuring BrowserFS for indexedDB: ${err}. Maybe your browser doesn't support indexedDB?`,
-          0,
-        ),
-      )
-      return
-    }
-    store.dispatch(actions.showMessage("BrowserFS ready"))
-    store.dispatch(actions.loadLocalStorage())
-  },
-)
-
-const git = (global.git = require("isomorphic-git"))
-const fs = (global.fs = require("fs"))
-const pfs = (global.pfs = require("pify")(fs))
-
-global.commitAll = async () => {
-  const repo = {
-    dir: store.getState().repo.dir,
-    fs,
-  }
-
-  const dir = `${repo.dir}/posts`
-  const entries = await pfs.readdir(dir)
-  await Promise.all(
-    entries.map(name =>
-      git.add({
-        ...repo,
-        filepath: `posts/${name}`,
-      }),
-    ),
-  )
-  await git.commit({
-    ...repo,
-    message: "autocommit",
-    author: {
-      name: "unijournal",
-      email: "unijournal@rtlong.com",
+function configureBrowserFS(dispatch) {
+  /* global BrowserFS */
+  BrowserFS.configure(
+    {
+      fs: "IndexedDB",
+      options: {},
     },
-  })
+    err => {
+      if (err) {
+        dispatch(
+          actions.showError(
+            `Error configuring BrowserFS for indexedDB: ${err}. Maybe your browser doesn't support indexedDB?`,
+            0,
+          ),
+        )
+        return
+      }
+      dispatch(actions.showMessage("BrowserFS ready"))
+      dispatch(actions.loadLocalStorage())
+    },
+  )
 }
+
+const store = createStore()
+
+const pfs = pify(fs)
+
+global.d = {
+  actions,
+  store,
+  fs,
+  git,
+  pfs,
+
+  async commitAll() {
+    const repo = {
+      dir: store.getState().repo.dir,
+      fs,
+    }
+
+    const dir = `${repo.dir}/posts`
+    const entries = await pfs.readdir(dir)
+    await Promise.all(
+      entries.map(name =>
+        git.add({
+          ...repo,
+          filepath: `posts/${name}`,
+        }),
+      ),
+    )
+    await git.commit({
+      ...repo,
+      message: "autocommit",
+      author: {
+        name: "unijournal",
+        email: "unijournal@rtlong.com",
+      },
+    })
+  },
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", () => mountApp(store))
+} else {
+  mountApp(store)
+}
+configureBrowserFS(store.dispatch)
